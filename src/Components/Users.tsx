64c27929d6e930f88533bba61f34a7566ecae68f
Nikita <mrbushik1@gmail.com>
2022-11-14 18:19:44 +0300

    dysplaung info about user by his id

--- src/Components/Users.tsx
+++ src/Components/Users.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export const Users: React.FC = () => {
+type UsersProps = {
+  currentCount: number;
+};
+type user = {
+  title?: string;
+  body?: string;
+};
+
+export const Users: React.FC<UsersProps> = ({ currentCount }) => {
+  const [userInfo, setUserInfo] = useState<user>({});
+
+  useEffect(() => {
+    console.log("change");
+    handleUpdate();
+  }, [currentCount]);
+
+  const handleUpdate = () => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${currentCount}`)
+      .then((response) => response.json())
+      .then((json) => setUserInfo(json));
+  };
   return (
     <div>
-
+      {Object.keys(userInfo) && (
+        <div>
+          <h3>{userInfo.title}</h3>
+          <p>{userInfo.body}</p>
+        </div>
+      )}
     </div>
   );
 };
-
--- src/Components/pages/infoPage.tsx
+++ src/Components/pages/infoPage.tsx
@@ -1,17 +1,38 @@
 import React, { useState } from "react";
+import { Users } from "../Users";
+import { NavBar } from "../navBar";
 
 const InfoPage: React.FC = () => {
   const [count, setCount] = useState(1);
 
   const handleMinus = (value: number) => {
+    if (!count) return;
     setCount((perv) => perv - value);
+  };
+
+  const handlePlus = (value: number) => {
+    setCount((perv) => perv + value);
   };
 
   return (
     <div>
+      <NavBar />
       <h1>Count {count}</h1>
-      <span>+</span>
-      <span onClick={() => handleMinus(1)}>-</span>
+      <div>
+        <span
+          onClick={() => handlePlus(1)}
+          style={{ fontSize: "40px", margin: "0 20px", cursor: "pointer" }}
+        >
+          +
+        </span>
+        <span
+          onClick={() => handleMinus(1)}
+          style={{ fontSize: "40px", cursor: "pointer" }}
+        >
+          -
+        </span>
+      </div>
+      <Users currentCount={count} />
     </div>
   );
 };
